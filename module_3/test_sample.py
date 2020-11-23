9c0ec5f9796c0be2a85fefaa559c60e5b9d46c94
OlegVR <olegrezvov89@gmail.com>
2020-11-23 14:38:55 +0400

    corrected the names of variables in the test_sample.py

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-import time
 
 
 def test_new_user_registration():
@@ -11,33 +10,27 @@ def test_new_user_registration():
         browser.implicitly_wait(5)
         browser.get(main_page_link)
 
-        browser.find_element_by_css_selector(search_button_login_or_registration_in_main_page_locator).click()
+        browser.find_element_by_css_selector(button_login_or_registration).click()
 
         # Act
-        search_input_new_user_email = browser.find_element_by_css_selector(
-            search_input_new_user_email_locator
-        )
-        search_input_new_user_email.send_keys(user_email)
+        input_email = browser.find_element_by_css_selector(input_email_locator)
+        input_email.send_keys(user_email)
 
-        search_input_new_user_password = browser.find_element_by_css_selector(
-            search_input_new_user_password_locator
-        )
-        search_input_new_user_password.send_keys(user_password)
+        input_password = browser.find_element_by_css_selector(input_password_locator)
+        input_password.send_keys(user_password)
 
-        search_input_repeat_password_new_user = browser.find_element_by_css_selector(
-            search_input_repeat_password_new_user_locator
-        )
-        search_input_repeat_password_new_user.send_keys(user_password)
-        browser.find_element_by_css_selector(search_button_registration_locator).click()
+        input_repeat_password = browser.find_element_by_css_selector(input_repeat_password_locator)
+        input_repeat_password.send_keys(user_password)
+
+        browser.find_element_by_css_selector(button_registration_locator).click()
 
         # Assert
-        search_thank_you_text = browser.find_element_by_css_selector(search_thank_you_text_locator).text
-        assert search_text in search_thank_you_text, "Registration page must contain the message of successful " \
+        success_login_msg_text = browser.find_element_by_css_selector(success_login_msg_text_locator).text
+        assert search_text in success_login_msg_text, "Registration page must contain the message of successful " \
                                                      "user registration."
         print("Successfully passed the first test!")
 
     finally:
-        time.sleep(5)
         browser.quit()
 
 
@@ -51,44 +44,40 @@ def test_log_in_as_a_registered_user():
         browser.get(login_and_registration_page_link)
 
         # Act
-        search_input_email_registered_user = browser.find_element_by_css_selector(
-            search_input_email_registered_user_locator
-        )
-        search_input_email_registered_user.send_keys(user_email)
+        input_email_registered = browser.find_element_by_css_selector(input_email_registered_locator)
+        input_email_registered.send_keys(user_email)
 
-        search_input_password_registered_user = browser.find_element_by_css_selector(
-            search_input_password_registered_user_locator
-        )
-        search_input_password_registered_user.send_keys(user_password)
-        browser.find_element_by_css_selector(search_button_login_registered_user_locator).click()
+        input_password_registered = browser.find_element_by_css_selector(input_password_registered_locator)
+        input_password_registered.send_keys(user_password)
+
+        browser.find_element_by_css_selector(button_login_registered_locator).click()
 
         # Assert
-        search_thank_you_text = browser.find_element_by_css_selector(search_thank_you_text_locator).text
-        assert search_text in search_thank_you_text, "Login page must contain a successful login message"
+        success_login_msg_text = browser.find_element_by_css_selector(success_login_msg_text_locator).text
+        assert search_text in success_login_msg_text, "Login page must contain a successful login message"
         print("Successfully passed the second test")
 
     finally:
-        time.sleep(5)
         browser.quit()
 
 
 if __name__ == "__main__":
     main_page_link = "http://selenium1py.pythonanywhere.com/ru/"
     login_and_registration_page_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    search_thank_you_text_locator = "div.alertinner.wicon"
+    success_login_msg_text_locator = "div.alertinner.wicon"
 
     user_password = "chupakabra124c"
-    user_email = "chupakabra6@au.nyam"
+    user_email = "chupakabra7@au.nyam"
 
-    search_button_login_or_registration_in_main_page_locator = "#login_link"
-    search_input_new_user_email_locator = "input[name='registration-email']"
-    search_input_new_user_password_locator = "input[name='registration-password1']"
-    search_input_repeat_password_new_user_locator = "input[name='registration-password2']"
-    search_button_registration_locator = "button[name='registration_submit']"
+    button_login_or_registration = "#login_link"
+    input_email_locator = "input[name='registration-email']"
+    input_password_locator = "input[name='registration-password1']"
+    input_repeat_password_locator = "input[name='registration-password2']"
+    button_registration_locator = "button[name='registration_submit']"
 
-    search_input_email_registered_user_locator = "input[name='login-username']"
-    search_input_password_registered_user_locator = "input[name='login-password']"
-    search_button_login_registered_user_locator = "button[name='login_submit']"
+    input_email_registered_locator = "input[name='login-username']"
+    input_password_registered_locator = "input[name='login-password']"
+    button_login_registered_locator = "button[name='login_submit']"
 
     test_new_user_registration()
     test_log_in_as_a_registered_user()
