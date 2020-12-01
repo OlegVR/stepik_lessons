a2cd595b3bd85a77489c468c73de7f989577e6c8
OlegVR <olegrezvov89@gmail.com>
2020-12-01 21:50:56 +0300

    Removed the dictionary from the file and added its import from a separate file, added a check for the presence of a key in the dictionary.

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,12 +1,4 @@
-CORRECT_LANGUAGES_DICT = {
-    "ru": "Добавить в корзину","ar": "إضافة إلى السلة", "ca": "Afegir a la cistella",
-    "cs": "Přidat do košíku", "da": "Tilføj til kurv", "de": "In den Warenkorb legen",
-    "en-gb": "Add to basket", "el": "Προσθήκη στο καλάθι", "es": "Añadir al carrito",
-    "fi": "Lisää ostoskoriin", "fr": "Ajouter au panier", "it": "Aggiungi al carrello",
-    "ko": "장바구니에 추가하기", "nl": "Toevoegen aan Carter", "pl": "Dodaj do karteru",
-    "pt": "Adicionar ao Cárter", "pt-br": "Adicionar ao Cárter", "ro": "Adaugă la cutie",
-    "sk": "Pridať do kľukovej skrine", "uk": "Додати в кошик", "sh-hans": "Lägg i kundvagnen"
-}
+from module_4.language_data import CORRECT_LANGUAGES_DICT
 
 
 def test_whether_the_add_product_to_cart_button(browser):
@@ -14,8 +6,11 @@ def test_whether_the_add_product_to_cart_button(browser):
     browser.implicitly_wait(5)
     browser.get(link)
 
-    text_button = browser.find_element_by_css_selector("button[class='btn btn-lg btn-primary btn-add-to-basket']").text
+    text_button = browser.find_element_by_css_selector("button.btn.btn-add-to-basket").text
     language = browser.find_element_by_css_selector("option[selected]").get_attribute("value")
+
+    assert language in CORRECT_LANGUAGES_DICT, \
+        f"Wrong language selected"
 
     assert CORRECT_LANGUAGES_DICT[language] in text_button,\
         f"The search text: {CORRECT_LANGUAGES_DICT[language]} not in the text: {text_button}"
