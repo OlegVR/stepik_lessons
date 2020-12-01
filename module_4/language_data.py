9583954379cf8a02c30aa09556f88ab15bc05cd7
OlegVR <olegrezvov89@gmail.com>
2020-12-01 23:38:08 +0300

    Created a file with correct verification data in the form of a dictionary.

--- module_4/language_data.py
+++ module_4/language_data.py
@@ -1,6 +1,7 @@
 CORRECT_LANGUAGES_DICT = {
     "ru": "Добавить в корзину",
-    "en-gb": "Add to basket",
+    "en-GB": "Add to basket",
     "es": "Añadir al carrito",
     "fr": "Ajouter au panier",
+    "en-gb": "Add to basket"
 }
