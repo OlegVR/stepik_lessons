f87c3874ecbfdee06dafaeaa9fbc808cbcbbec7a
OlegVR <olegrezvov89@gmail.com>
2020-12-04 21:24:12 +0300

    Deleted file language_data.py'
    q

--- module_4/language_data.py
+++
@@ -1,7 +0,0 @@
-CORRECT_LANGUAGES_DICT = {
-    "ru": "Добавить в корзину",
-    "en-GB": "Add to basket",
-    "es": "Añadir al carrito",
-    "fr": "Ajouter au panier",
-    "en-gb": "Add to basket"
-}
