473f76edbdbd76626ea05553d69af221f445cb44
OlegVR <olegrezvov89@gmail.com>
2020-12-11 01:47:31 +0300

    Added test cases test_guest_should_see_login_link_on_product_page and test_guest_can_go_to_login_page_from_product_page test testproductpage file test_product_page.py main home page bas base page.

--- module_5/pages/login_page.py
+++ module_5/pages/login_page.py
@@ -17,3 +17,14 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "There is no registration form on the page"
 
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.INPUT_EMAIL)
+        email_input.send_keys(email)
+        password_input = self.browser.find_element(*LoginPageLocators.INPUT_PASSWORD)
+        password_input.send_keys(password)
+        password_replay = self.browser.find_element(*LoginPageLocators.INPUT_PASSWORD_REPLY)
+        password_replay.send_keys(password)
+        button_register = self.browser.find_element(LoginPageLocators.BUTTON_REGISTER)
+        button_register.click()
+
+
