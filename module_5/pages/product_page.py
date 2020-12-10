d12a85966efc05231d3f28c3d1578cc4e5cacbd8
OlegVR <olegrezvov89@gmail.com>
2020-12-11 01:00:17 +0300

    Added test cases for checking the product page. Added a negative test, check the availability on the text page adding to cart after you click add products, the added positive test for the absence of text stating that the product is added to cart when you go to the product page without performing any additional steps, added a negative test verification loss of text about what the item is added to cart after adding a product to cart. Added methods for interacting with the Product Page.

--- module_5/pages/product_page.py
+++ module_5/pages/product_page.py
@@ -1,32 +1,38 @@
 from .base_page import BasePage
-from .locators import ProductPageLoctors
+from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
     def add_product_to_the_bascket(self):
-        product_name = self.browser.find_element(*ProductPageLoctors.PRODUCT_NAME).text
-        cost_product = self.browser.find_element(*ProductPageLoctors.COST_PRODUCT).text
+        button = self.browser.find_element(*ProductPageLocators.SUBMIT_ADD_TO_BASKET)
+        button.click()
 
-        self.browser.find_element(*ProductPageLoctors.SUBMIT_ADD_TO_BASKET).click()
+    def product_name_on_the_page(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
 
-        self.solve_quiz_and_get_code()
-        self.should_add_to_basket_message()
-        self.check_is_product_in_message(product_name)
-        self.should_message_cost_of_a_basket()
-        self.check_cost_product_in_message(cost_product)
+    def cost_product_on_the_page(self):
+        return self.browser.find_element(*ProductPageLocators.COST_PRODUCT).text
 
     def should_add_to_basket_message(self):
-        assert self.is_element_present(*ProductPageLoctors.ADD_TO_BASKET_MESSAGE), \
-            'The page does not contain the message adding an item to the cart'
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_MESSAGE), \
+            'The page does not contain the message adding an item to the basket'
 
     def check_is_product_in_message(self, product_name):
-        assert product_name == self.browser.find_element(*ProductPageLoctors.NAME_PRODUCT_IN_MESSAGE).text, \
+        assert product_name == self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_IN_MESSAGE).text, \
             "Product names don't match"
 
     def should_message_cost_of_a_basket(self):
-        assert self.is_element_present(*ProductPageLoctors.MESSAGE_COST_IN_BASKET), \
-            "No message about the cost of the shopping cart"
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_COST_IN_BASKET), \
+            "No message about the cost of the shopping basket"
 
     def check_cost_product_in_message(self, cost_product):
-        assert cost_product == self.browser.find_element(*ProductPageLoctors.COST_IN_MESSAGE).text, \
+        assert cost_product == self.browser.find_element(*ProductPageLocators.COST_IN_MESSAGE).text, \
             "The cost of the product in the message does not match"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented"
--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -1,23 +1,117 @@
 import pytest
+import time
 
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
+from .pages.locators import LoginPageLocators
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+from .data import data
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param(
-                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                      marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 class TestProductPage:
+    @pytest.mark.parametrize('link',
+                             ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                              pytest.param(
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  marks=pytest.mark.xfail),
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
     def test_guest_can_add_product_to_basket(self, browser, link):
         page = ProductPage(browser, link)
         page.open()
+
+        name_product = page.product_name_on_the_page()
+        cost_product = page.cost_product_on_the_page()
+
         page.add_product_to_the_bascket()
+        page.solve_quiz_and_get_code()
+
+        page.should_add_to_basket_message()
+        page.check_is_product_in_message(name_product)
+        page.should_message_cost_of_a_basket()
+        page.check_cost_product_in_message(cost_product)
+
+    @pytest.mark.xfail
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+        page = ProductPage(browser, link)
+        page.open()
+
+        page.add_product_to_the_bascket()
+
+        page.should_not_be_success_message()
+
+    def test_quest_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+
+        page = ProductPage(browser, link)
+        page.open()
+
+        page.should_not_be_success_message()
+
+    @pytest.mark.xfail
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+
+        page = ProductPage(browser, link)
+        page.open()
+
+        page.add_product_to_the_bascket()
+
+        page.should_be_success_message()
+
+#    def test_guest_should_see_login_link_on_product_page(self, browser, link):
+#        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#        page = ProductPage(browser, link)
+#        page.open()
+#        page.should_be_login_link()
+#
+#    def test_guest_can_go_to_login_page_from_product_page (self, browser, link):
+#        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#        page = ProductPage(browser, link)
+#        page.open()
+#        page.go_to_login_page()
+#        assert page.is_element_present(*LoginPageLocators.LOGIN_FORM) and \
+#               page.is_element_present(*LoginPageLocators.REGISTER_FORM), "Not on the page LOGIN_FORM or REGISTER_FORM"
+
+#    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser, link):
+#        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#        page = ProductPage(browser, link)
+#        page.open()
+#        page.go_to_basket()
+#        basket_pages = BasketPage(browser, browser.current_url)
+#        basket_pages.is_empty_basket(message=data['en'])
+
+
+#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"
+
+
+#class TestUserAddToBasketFromProductPage:
+#    @pytest.fixture(scope="function", autouse=True)
+#    def setup(self):
+#        self.link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"
+#        page = LoginPage(browser, link)
+#        page.go_to_login_page()
+#        email = str(time.time()) + "@fakemail.org"
+#        password = "passnumber1"
+#        page.register_new_user(email, password)
+#        page.should_be_authorized_user()
+#
+#    def test_user_cant_see_success_message(self, browser):
+#        page = ProductPage(browser, self.link)
+#        page.open()
+#        assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+#            "Success message is presented, but should not be"
+#
+#    def test_user_can_add_product_to_basket(self, browser):
+#        page = ProductPage(browser, self.link)
+#        page.open()
+#        page.add_product_to_the_bascket()
 
