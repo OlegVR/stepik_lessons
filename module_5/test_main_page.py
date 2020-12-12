e408d6a652ec7b73afd3ec13c06f5940aac00b8b
OlegVR <olegrezvov89@gmail.com>
2020-12-12 11:58:44 +0300

    Import refactoring

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,5 +1,8 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+from .data import data
+import time
 
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -17,4 +20,11 @@ class TestMainPage:
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
+
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_basket()
+        basket_pages = BasketPage(browser, browser.current_url)
+        basket_pages.is_empty_basket(message=data['en'])
 
