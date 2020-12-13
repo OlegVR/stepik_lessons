d69e3b80aa39ef4673e28b703b1875b5ed7a0697
OlegVR <olegrezvov89@gmail.com>
2020-12-13 17:55:34 +0300

    product_page.py

--- module_5/pages/product_page.py
+++ module_5/pages/product_page.py
@@ -21,21 +21,26 @@ class ProductPage(BasePage):
             'The page does not contain the message adding an item to the basket'
 
     def check_is_product_in_message(self, product_name):
+
         assert product_name == self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_IN_MESSAGE).text, \
             "Product names don't match"
 
     def should_message_cost_of_a_basket(self):
+
         assert self.is_element_present(*ProductPageLocators.MESSAGE_COST_IN_BASKET), \
             "No message about the cost of the shopping basket"
 
     def check_cost_product_in_message(self, cost_product):
+
         assert cost_product == self.browser.find_element(*ProductPageLocators.COST_IN_MESSAGE).text, \
             "The cost of the product in the message does not match"
 
     def should_not_be_success_message(self):
+
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
 
     def should_be_success_message(self):
+
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented"
